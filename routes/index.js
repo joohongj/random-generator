bc31d5f24ec35f13244fd4022b454a92b332054f
Joohong Jung <joohongj@gmail.com>
2026-05-06 23:56:27 -0400

    updated

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function (req, res, next) {
+const quotes = require('../data/data.json');
+
+router.get('/', function (req, res) {
+
     // 1. Read the data.json file
     // 2. Pick a random item from the data.json list
     // 3. Render the index.hbs template with the random item.
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const randomQuote = quotes[randomIndex];
 
     res.render('index', {
-        // your variables go here
+        name: randomQuote.name,
+        quote: randomQuote.quote
     });
 });
 
+router.get('/quote', function (req, res) {
+
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const randomQuote = quotes[randomIndex];
+
+    res.json(randomQuote);
+});
+
+
 module.exports = router;
+
